5a6b57a2155b224434d6b3642bce474bdfb0a6f9
Halfboiled720p <nadarramadhani8@gmail.com>
2024-10-15 21:44:44 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/prakmobil/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.prakmobil
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screens/recipes_detail_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import '../models/meal.dart';
+import 'webview_screen.dart';
+
+class RecipeDetailScreen extends StatelessWidget {
+  final Meal meal;
+
+  RecipeDetailScreen({required this.meal});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(meal.name),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(meal.thumbnailUrl),
+            SizedBox(height: 8),
+            Text(
+              'Category: ${meal.category ?? 'Unknown'}',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Instructions:',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 4),
+            Text(
+              meal.instructions ?? 'Tidak ada instruksi.',
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 8),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => WebViewScreen(
+                      url: meal.url,
+                    ),
+                  ),
+                );
+              },
+              child: Text('Lihat di Web'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
